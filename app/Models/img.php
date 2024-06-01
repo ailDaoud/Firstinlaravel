9fac6ef3c627f77a3026be8077d89dafd86860c6
ailDaoud <daoudali259@gmail.com>
2024-06-01 20:08:37 +0300

    fix the Img class

--- app/Models/img.php
+++ app/Models/img.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class img extends Model
+class Img extends Model
 {
     use HasFactory;
 }
